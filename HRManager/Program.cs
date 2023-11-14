003b81d48c218d910d7ae7d1e185add0db49f975
joshbivens <thejoshbivens@gmail.com>
2023-11-14 10:27:21 -0700

    Add project files.

---
+++ HRManager/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.EmpNum = 1;
+            emp.FirstName = "Steve";
+            emp.LastName = "Jobs";
+            Console.WriteLine("emp.GetHashCode: " + emp.GetHashCode());
+            Console.WriteLine("emp.GetType: " + emp.GetType());
+            Console.WriteLine("emp.ToString: " + emp.ToString());
+
+            HourlyEmployee hourEmp = new HourlyEmployee();
+            hourEmp.EmpNum = 2;
+            hourEmp.FirstName = "Bill";
+            hourEmp.LastName = "Gates";
+            hourEmp.HourlyRate = 15.00M;
+            Console.WriteLine("hourEmp.GetHashCode: " + hourEmp.GetHashCode());
+            Console.WriteLine("hourEmp.GetType: " + hourEmp.GetType());
+            Console.WriteLine("hourEmp.ToString: " + hourEmp.ToString());
+
+            Object athing = hourEmp;
+            Console.WriteLine("hourEmp.ToString: " + athing);
+
+            if (athing is HourlyEmployee)
+            {
+                //Unboxing
+                HourlyEmployee unboxedEmp = (HourlyEmployee)athing;
+                Console.WriteLine("First Name: " + unboxedEmp.FirstName);
+            }
+
+            if (athing is SalaryEmployee)
+            {
+                //Unboxing
+                SalaryEmployee unboxedEmp = (SalaryEmployee)athing;
+                Console.WriteLine("First Name: " + unboxedEmp.FirstName);
+            }
+
+        }
+    }
+}
